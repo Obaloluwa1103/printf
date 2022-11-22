0b776f11efac30032c4e7a8a7a20a7e061308d57
Angie <angie.monnye@gmail.com>
2022-11-22 09:53:16 +0200

    Hot fix

--- _specifiers.c
+++ _specifiers.c
@@ -108,7 +108,7 @@ int print_percent(va_list types, char buffer[],
  * Return: Number of chars printed
  */
 
-nt print_int(va_list types, char buffer[],
+int print_int(va_list types, char buffer[],
 	int flags, int width, int precision, int size)
 {
 	int i = BUFF_SIZE - 2;
